ebc405c9efa8d2d5533b3812426b0d2b98a561bd
James NixonInnes <nixoninnes@gmail.com>
2016-02-07 20:20:22 +0000

    update funcs

--- funcs.py
+++ funcs.py
@@ -1,3 +1,4 @@
+import requests
 import mud
 import db
 from actions import actions
@@ -10,24 +11,23 @@ def login(client, args):
 
     if args[0] == 'register':
         if len(args) == 4 and args[2] == args[3]:
-            if client.session.query(db.User).filter_by(name=args[1]).first() is None:
+            if db.session.query(db.User).filter_by(name=args[1]).first() is None:
                 client.user = db.User(name=args[1], password=args[2])
-                client.session.add(client.user)
-                client.session.commit()
-                mud.users.append(client.user)
-                client.authd = True
+                db.session.add(client.user)
+                db.session.commit()
                 actions['goto'](client, ['Lobby'])
+                client.get_prompt()
                 return
             else:
                 client.msg_self("Username \"{}\" is already taken, sorry.\n".format(args[1]))
 
     if len(args) == 2:
-        dbuser = client.session.query(db.User).filter_by(name=args[0]).first()
+        dbuser = db.session.query(db.User).filter_by(name=args[0]).first()
         if dbuser is not None:
             if dbuser.verify_password(args[1]):
                 client.user = dbuser
-                client.authd = True
                 actions['goto'](client, ['Lobby'])
+                client.get_prompt()
                 return
 
     client.msg_self("Login error.\nLogin: <username> <password>\nNew Account: register <username> <password> <password>\n")
@@ -39,8 +39,34 @@ def get_room(room_name):
             return room
     return None
 
-def get_user(user_name):
-    for user in mud.users:
-        if user.name == user_name:
-            return user
+def get_client(user_name):
+    for client in mud.clients:
+        if client.user.name == user_name:
+            return client
     return None
+
+def update_cards():
+    cards = requests.get('http://mtgjson.com/json/AllCards.json').json()
+    for c in cards:
+        #try:
+        card = db.Card(
+            name = cards[c]['name'],
+            names = cards[c]['names'] if 'names' in cards[c] else None,
+            manaCost = cards[c]['manaCost'] if 'manaCost' in cards[c] else None,
+            cmc = cards[c]['cmc'] if 'cmc' in cards[c] else None,
+            colors = cards[c]['colors'] if 'colors' in cards[c] else None,
+            type = cards[c]['type'],
+            supertypes = cards[c]['supertypes'] if 'supertypes' in cards[c] else None,
+            types = cards[c]['types'] if 'types' in cards[c] else None,
+            subtypes = cards[c]['subtypes'] if 'subtypes' in cards[c] else None,
+            rarity = cards[c]['rarity'] if 'rarity' in cards[c] else None,
+            text = cards[c]['text'] if 'text' in cards[c] else None,
+            power = cards[c]['power'] if 'power' in cards[c] else None,
+            toughness = cards[c]['toughness'] if 'toughness' in cards[c] else None,
+            loyalty = cards[c]['loyalty'] if 'loyalty' in cards[c] else None
+        )
+        db.session.add(card)
+        #except:
+        #    print("Card: {} failed to be saved.".format(cards[c]['name']))
+    db.session.commit()
+
